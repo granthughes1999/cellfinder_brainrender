7c8685c212c6f5c907e9a8b3863662107aa7c458
granthughes1999 <grant.2.hughes@cuanschutz.edu>
2023-01-13 20:01:37 -0700

    these are the variables that you want to use in all of these files

---
+++ variables.py
@@ -0,0 +1,14 @@
+
+# Update these variables
+
+# 1. ID of mouse (example: G25)
+mouse_id = "test_001"
+
+# 2. cellfinder_output_path = 'path/to/root/directory/of/cellfinder/output/data/'
+cellfinder_output_path = "/Users/grant/Desktop/mock_df/cellfinder_output/"
+
+# 3. How many top brain regions to evaluate. (default is 5)
+brain_regions_to_evalutate = 5
+
+# 4. path to local location of allen mouse brain atlas
+allen_mouse_10um = '/Users/grant/brainglobe/allen_mouse_10um'
