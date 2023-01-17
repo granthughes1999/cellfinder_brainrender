9b6926b63c64e7406e122ffea285435d279c1b84
granthughes1999 <grant.2.hughes@cuanschutz.edu>
2023-01-17 10:45:04 -0700

    updated imports to match new file names.

--- analyze_cellfinder_data.py
+++ analyze_cellfinder_data.py
@@ -4,8 +4,8 @@
 # (cellfinder2) path/to/run_to_analyze_cellfinder_data
 # You may need to downgrade numpy inisde this enviroment to get the file to run
 
-import variables
-from variables import cellfinder_output_path, mouse_id, brain_regions_to_evalutate, allen_mouse_10um
+import UpdateME
+from UpdateME import cellfinder_output_path, mouse_id, brain_regions_to_evalutate, allen_mouse_10um
 import analyze_cellfinder_data
 from analyze_cellfinder_data import analyze_data_cellfinder
 
--- create_3d_brain_render.py
+++ create_3d_brain_render.py
@@ -1,9 +1,9 @@
 # Run this file inside an Anaconda env with brainrender installed 
 # (brainrender) python path/to/run_to_create_3D_render.py
 
-import variables
-from variables import cellfinder_output_path, mouse_id, brain_regions_to_evalutate, allen_mouse_10um
-import brainrender_mac_combined
-from brainrender_mac_combined import run_brainrender
+import UpdateME
+from UpdateME import cellfinder_output_path, mouse_id, brain_regions_to_evalutate, allen_mouse_10um
+import brainrender_backend
+from brainrender_backend import run_brainrender
 
 run_brainrender(cellfinder_output_path, mouse_id, brain_regions_to_evalutate, allen_mouse_10um)
