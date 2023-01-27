2c36afe3bf1db4de383286555e1866361f5c07af
granthughes1999 <grant.2.hughes@cuanschutz.edu>
2023-01-27 13:51:37 -0700

    general_imports.py
    moved all imports to general_imports.py, and added from general_imports import *
    
    makes code cleaner

--- brainrender_backend.py
+++ brainrender_backend.py
@@ -1,41 +1,4 @@
-# General Imports
-import numpy as np
-import os
-import pandas as pd
-import json
-from pathlib import Path
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pickle 
-from scipy.spatial import distance
-from rich import print
-from myterial import orange
-# brainrender files/function imports
-import brainrender
-from brainrender.scene import Scene
-from brainrender.actors import Points
-from brainrender.actors import PointsDensity
-from brainrender.actor import Actor
-from brainrender import settings
-from brainrender import Scene
-from brainrender.actors import Cylinder
-brainrender.SHADER_STYLE = "cartoon"
-# vedo imports
-from vedo import Spheres, Sphere
-from vedo import Points as vPoints
-# bg_atlasapi imports
-from bg_atlasapi.bg_atlas import BrainGlobeAtlas
-import bg_space as bg
-from bg_atlasapi import show_atlases
-from bg_atlasapi.bg_atlas import BrainGlobeAtlas
-# files and variables from this repo imports
-import UpdateME
-from UpdateME import cellfinder_output_path, mouse_id, brain_regions_to_evalutate, allen_mouse_10um, estim_tip_coordinates, extra_brain_region_acryonm, estim_shank_radius_um, estim_tip_radius_um, estim_propigation_radius_um, opticalfiper_radius_um, opticalfiber_tip_coordinates,opticalfiber_propigation_radius_um
-import cellfinder_backend
-from cellfinder_backend import analyze_data_cellfinder
-from distance_calculations_and_histograms import distance_calculations_histograms
-from vector_calculations import estim_cell_coordinates
-from shared_cells_distance_calculations import shared_cell_distance_calculations_histograms
+from general_Imports import *
 
 
 def run_brainrender(cellfinder_output_path, mouseid, brain_regions_to_evalutate, allen_mouse_10um,estim_shank_radius_um,estim_tip_radius_um,estim_propigation_radius_um):
@@ -248,9 +211,9 @@ def run_brainrender(cellfinder_output_path, mouseid, brain_regions_to_evalutate,
  
 
     # add top brain regions and labels
-    colors = ["red", 'orange', "yellow", "green", "blue", "red", 'orange',
-              "yellow", "green", "blue", "red", 'orange', "yellow", "green", "blue","red", 'orange', "yellow", "green", "blue", "red", 'orange',
-              "yellow", "green", "blue", "red", 'orange', "yellow", "green", "blue"]
+    # colors = ["red", 'orange', "yellow", "green", "blue", "red", 'orange',
+    #           "yellow", "green", "blue", "red", 'orange', "yellow", "green", "blue","red", 'orange', "yellow", "green", "blue", "red", 'orange',
+    #           "yellow", "green", "blue", "red", 'orange', "yellow", "green", "blue"]
     for i in range(brain_regions_to_evalutate):
         evaluate_brain_region_acronyms[i] = scene.add_brain_region(
             str(evaluate_brain_region_acronyms[i]), alpha=0.2, color='blue')
---
+++ general_Imports.py
@@ -0,0 +1,38 @@
+# General Imports
+import numpy as np
+import os
+import pandas as pd
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle 
+from scipy.spatial import distance
+from rich import print
+from myterial import orange
+# brainrender files/function imports
+import brainrender
+from brainrender.scene import Scene
+from brainrender.actors import Points
+from brainrender.actors import PointsDensity
+from brainrender.actor import Actor
+from brainrender import settings
+from brainrender import Scene
+from brainrender.actors import Cylinder
+brainrender.SHADER_STYLE = "cartoon"
+# vedo imports
+from vedo import Spheres, Sphere
+from vedo import Points as vPoints
+# bg_atlasapi imports
+from bg_atlasapi.bg_atlas import BrainGlobeAtlas
+import bg_space as bg
+from bg_atlasapi import show_atlases
+from bg_atlasapi.bg_atlas import BrainGlobeAtlas
+# files and variables from this repo imports
+import UpdateME
+from UpdateME import cellfinder_output_path, mouse_id, brain_regions_to_evalutate, allen_mouse_10um, estim_tip_coordinates, extra_brain_region_acryonm, estim_shank_radius_um, estim_tip_radius_um, estim_propigation_radius_um, opticalfiper_radius_um, opticalfiber_tip_coordinates,opticalfiber_propigation_radius_um
+import cellfinder_backend
+from cellfinder_backend import analyze_data_cellfinder
+from distance_calculations_and_histograms import distance_calculations_histograms
+from vector_calculations import estim_cell_coordinates
+from shared_cells_distance_calculations import shared_cell_distance_calculations_histograms
